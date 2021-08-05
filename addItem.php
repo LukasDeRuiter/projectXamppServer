caae6e5c23aad7c34b40edd821d39d437d84fe45
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-05 12:10:29 +0200

    added form page

---
+++ addItem.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('templates/header.php') ?>
+
+<form class="addPokemonForm">
+<label class="formLabel">Name your pokedéx entry:</label>
+<input type="text" name="name" class="pokeInputField">
+<label class="formLabel">Give its number:</label>
+<input type="text" name="number" class="pokeInputField">
+<label class="formLabel">Add type:</label>
+<input type="text" name="type" class="pokeInputField">
+<input type="submit" name="submit" value="submit" class="submitBtn">
+
+
+
+</form>
+
+
+<?php include('templates/footer.php') ?>
+
+</html>
