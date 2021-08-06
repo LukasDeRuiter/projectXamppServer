8187a3a90568218c07c1868d0a81f215681d40ca
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-06 09:44:44 +0200

    added db connection

--- index.php
+++ index.php
@@ -12,11 +12,22 @@ function squareNumber($number) {
     return pow($number, 2);
 }
 
-$connection = mysqli_connect('localhost', 'lukasderuiter', 'thisisreplacedforsafety', 'project_pokedex');
+$connection = mysqli_connect('localhost', 'lukasderuiter', 'replaced', 'project_pokedex');
 
 if(!$connection) {
     echo "error: " . mysqli_connect_error();
 }
+
+$sql = "SELECT * FROM pokemon";
+
+$result = mysqli_query($connection, $sql);
+
+$fullPokedex = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+mysqli_free_result($result);
+mysqli_close($connection);
+
+print_r($fullPokedex);
 
 ?>
 
